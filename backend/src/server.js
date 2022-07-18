911ff6858c3f340c5ada38984f859f686051e218
mobius29 <sagjin0000@naver.com>
2022-07-18 13:35:25 +0000

    0718

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,21 +6,24 @@ const HashMap = require('hashmap')
 const server = createServer(app)
 const wss = new WebSocket.Server({ server })
 
-let hashmap = new HashMap()
+let socket_hashmap = new HashMap()
 
-const max_client_count = 2
+const max_client_count = 3
 const random_motion_count = 3
-const random_motion_start = 20
-const random_motion_during = 11
+const random_motion_start = 5
+const random_motion_during = 6
+const game_start_countdown = 3
+const die_count = 3
+const npc_count = 4
 
-let random_motion_time =
-  parseInt(Math.random() * random_motion_during) + random_motion_start
+let count = 0 // 1초동안 들어온 소켓의 개수 저장.
 let client_count = 0 // 서버에 들어온 소켓의 수 저장.
 let client_id = 0
-let count = 0 // 1초동안 들어온 소켓의 개수 저장.
 let user_info = {} // 유저 소켓을 저장할 객체. { id: ws value }
 let received_strings = [] // 유저들이 소켓을 보낼 때, 들어온 문자열들을 저장할 배열. 나중에 join 메소드를 통해 슬래시(/)로 연결할 예정.
+let flag_info = {}
 
+/*
 const toBytes = (str) => {
   const buffer = Buffer.from(str, 'utf8')
   const result = Array(buffer.length)
@@ -29,10 +32,16 @@ const toBytes = (str) => {
   }
   return result
 }
+*/
+
+const randomMotion = (time, id) => {
+  console.log(time, id)
+  sendBroadcastMessage('random_move', time + '_' + id)
+}
 
 const sendBroadcastMessage = (eventName, message) => {
   const sendMessage = eventName + '!' + message
-  hashmap.forEach((value, key) => {
+  socket_hashmap.forEach((value, key) => {
     value.send(sendMessage)
   })
 }
@@ -49,39 +58,78 @@ const allUserSendPosition = () => {
 
 const parseStringAndSendData = (str) => {
   const splittedString = str.split('!')
-  console.log(splittedString)
+  // console.log(splittedString)
   if (splittedString[0] === 'unit_position') {
     const id = splittedString[1].charAt(0)
 
     if (user_info[id] === undefined) {
-      user_info[id] = splittedString[1]
+      user_info[id] = splittedString[1][0]
+      const splitted_info = splittedString[1].split(';')
       received_strings = received_strings.concat(splittedString[1])
+
+      // flag 초기화
+      for (let i = 0; i < splitted_info.length; ++i) {
+        const splitted_unit = splitted_info[i].split(',')
+        if (flag_info[splitted_unit[0]] === undefined) {
+          flag_info[splitted_unit[0]] = 0
+        }
+      }
 
       if (Object.keys(user_info).length === client_count) {
         allUserSendPosition()
       }
     }
+  } else if (splittedString[0] === 'hit') {
+    const unit_id = splittedString[1]
+    console.log(unit_id)
+
+    if (unit_id === 'Wall(Clone)' || unit_id === 'WallTileMap') return
+
+    ++flag_info[unit_id]
+    console.log(flag_info[unit_id] + ' | ' + unit_id.toString())
+    if (flag_info[unit_id] >= die_count) {
+      sendBroadcastMessage('die', unit_id.toString())
+    }
+  } else if (splittedString[0] === 'zoomout') {
+    console.log('zoom out is completed')
+    const time =
+      parseInt(Math.random() * random_motion_during) + random_motion_start
+    const random_motion_id = parseInt(
+      Math.random() * random_motion_count
+    ).toString()
+    randomMotion(time, random_motion_id)
+    // setTimeout(() => randomMotion(time), time * 1000)
+  } else {
+    console.log(splittedString[0])
   }
 }
+
 wss.on('connection', (ws) => {
   console.log('client joined.')
   const socket_id = client_id++
   client_count++
-  hashmap.set(socket_id, ws) // socket id를 hashmap에 저장
+  socket_hashmap.set(socket_id, ws) // socket id를 hashmap에 저장
+
+  flag_info[socket_id.toString()] = 0
+  for (let i = 0; i < npc_count; ++i) {
+    const npc_id = socket_id + '_' + i
+    flag_info[npc_id] = 0
+  }
 
   ws.send(`id_set!${socket_id}`) // 유저에게 socket id 전달 -> 앞으로 들어올 문자열 제일 앞에 들어올 예정
   console.log(socket_id)
 
   if (client_count === max_client_count) {
     sendBroadcastMessage('count_down', '.')
-    setInterval(() => {
-      const random_motion_id = parseInt(
-        Math.random() * random_motion_count
-      ).toString()
-      random_motion_time =
-        parseInt(Math.random() * random_motion_during) + random_motion_start
-      sendBroadcastMessage('random_motion', random_motion_id)
-    }, random_motion_time * 1000)
+    const time =
+      parseInt(Math.random() * random_motion_during) + random_motion_start
+    const random_motion_id = parseInt(
+      Math.random() * random_motion_count
+    ).toString()
+    setTimeout(
+      () => randomMotion(time, random_motion_id),
+      (time + game_start_countdown) * 1000
+    )
   }
 
   ws.on('message', (data) => {
@@ -110,10 +158,10 @@ wss.on('connection', (ws) => {
   */
 
   ws.on('close', () => {
-    const closedKey = hashmap.search(ws)
+    const closedKey = socket_hashmap.search(ws)
     client_count--
     console.log(closedKey)
-    hashmap.delete(closedKey)
+    socket_hashmap.delete(closedKey)
     console.log('client left.')
   })
 })
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,104 +1,19 @@
-import {useEffect, useState} from 'react';
+import {Unity, useUnityContext} from 'react-unity-webgl';
 import './App.css';
-import IngamePage from './pages/IngamePage';
-import {NodeGroup} from 'react-move';
-import {easeExpInOut} from 'd3-ease';
-import {interpolate, interpolateTransformSvg} from 'd3-interpolate';
-import {scaleBand} from 'd3-scale';
 
-const randomPlayers = () => {
-	const ret = [];
-	for (let i = 1; i <= 20; ++i) {
-		const new_ret = ret.concat({
-			id: i,
-			color: '#' + (Math.random() * 0xffffff).toString(16),
-			pos: {
-				x: parseInt(Math.random() * 780) + 10,
-				y: parseInt(Math.random() * 580) + 10,
-			},
-		});
-	}
-};
-
-const App = () => {
-	const view = [800, 600];
-	const trbl = [10, 10, 10, 10];
-
-	const dims = [view[0] - trbl[1] - trbl[3], view[1] - trbl[0] - trbl[2]];
-	const [players, setPlayers] = useState(randomPlayers);
-
-	const update = () => {
-		setPlayers(randomPlayers);
-	};
-
-	/*
-	useEffect(() => {
-		const isKeyPressed = keyCode => {
-			console.log(keyCode);
-			setKeyPressed({
-				[keyCode]: true,
-				...keyPressed,
-			});
-		};
-
-		const movePosition = key => {
-			console.log(keyPressed);
-			if (key === 'w') {
-				setPos({x: pos.x - d, y: pos.y});
-			}
-			if (key === 'd') {
-				setPos({x: pos.x, y: pos.y + d});
-			}
-		};
-
-		const isKeyReleased = keyCode => {
-			setKeyPressed({
-				[keyCode]: false,
-				...keyPressed,
-			});
-		};
-
-		const onKeyDown = e => {
-			if (keyPressed[87] === true && keyPressed[68] === true) {
-				console.log('!23123');
-				setPos({x: pos.x - d, y: pos.y + d});
-				return;
-			}
-
-			if (e.key === 'w') {
-				console.log('11111111111111111');
-				movePosition(e.key);
-				isKeyPressed(e.keyCode);
-
-				console.log('222222222222222222');
-			}
-			if (e.key === 's') setPos({x: pos.x + d, y: pos.y});
-			if (e.key === 'd') {
-				movePosition(e.key);
-				isKeyPressed(e.keyCode);
-			}
-		};
-
-		const onKeyRelease = e => {
-			console.log(e.key);
-			isKeyReleased(e.keyCode);
-		};
-
-		document.addEventListener('keydown', onKeyDown);
-		document.addEventListener('keyup', onKeyRelease);
-
-		return () => {
-			document.removeEventListener('keydown', onKeyDown);
-			document.removeEventListener('keyup', onKeyRelease);
-		};
-	}, [players]);
-	*/
+function App() {
+	const {unityProvider} = useUnityContext({
+		loaderUrl: 'Build/build.loader.js',
+		dataUrl: 'Build/build.data',
+		frameworkUrl: 'Build/build.framework.js',
+		codeUrl: 'Build/build.wasm',
+	});
 
 	return (
-		<div>
-			<button onClick={update}>update</button>
+		<div className="App">
+			<Unity className="unity" unityProvider={unityProvider} />
 		</div>
 	);
-};
+}
 
 export default App;
